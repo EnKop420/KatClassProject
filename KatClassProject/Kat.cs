de524663fb6d601b5fc3b566ba0f7aeeba60c241
EnKop420 <rasmushww@gmail.com>
2022-11-30 08:42:45 +0100

    Add project files.

---
+++ KatClassProject/Kat.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KatClassProject
+{
+    public class Kat
+    {
+        public string Farve { get; set; }
+
+        public Kat(string farve)
+        {
+            Farve = farve;
+        }
+        public string changeColor()
+        {
+            return "gul";
+        }
+    }
+}
---
+++ KatClassProject/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using KatClassProject;
+
+Kat katFarve = new("grå");
+
+Console.WriteLine("farven på katten er " + katFarve.Farve);
+Console.WriteLine("Efter lidt tid blev katten " + katFarve.changeColor());
